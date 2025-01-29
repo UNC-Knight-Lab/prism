276833315bb4d80c2a4e57b759198114d25902f7
s-chittari <91336143+s-chittari@users.noreply.github.com>
2025-01-29 15:10:19 -0500

    updated test file with callouts

---
+++ bridgette.py
@@ -0,0 +1,25 @@
+from fitting_functions.traditional_methods import MeyerLoweryFitting
+from fitting_functions.ODE_solving import PetRAFTKineticFitting
+from fitting_functions.traditional_methods import MeyerLoweryFitting
+import pandas as pd
+
+# read in data
+peter_data = pd.read_excel('/Users/suprajachittari/Documents/peter/sequence/PDB-4-098_conversion.xlsx', header=0)
+
+# initialize fitting object
+f = PetRAFTKineticFitting(peter_data, 97.95, 34.29) # for Peter
+
+##### functions to assist in fitting and visualizing data #
+
+# visualize the conversion plot with just some test values of r_1 and r_2
+f.test_values(1, 1)
+
+# display the overlay of your data with a test 
+f.display_overlay([1, 1])
+
+# extract rates using guess data
+f.extract_rates(0.5, 10)
+
+# extract data using Meyer Lowery fitting
+f = MeyerLoweryFitting()
+f.extract_rates(peter_data)
--- fitting_functions/ODE_solving.py
+++ fitting_functions/ODE_solving.py
@@ -83,9 +83,9 @@ class PetRAFTKineticFitting():
         return np.sum(residuals**2)
     
     def _objective(self, k):
-        k_AB, k_BA = k
-        k_AA = 1.
-        k_BB = 1.
+        k_AA, k_BB = k
+        k_AB = 1.
+        k_BA = 1.
 
         sol = self._integrate_ODE(k_AA, k_AB, k_BA, k_BB)
         pred_F, pred_X = self._convert_XF(sol)
@@ -94,9 +94,9 @@ class PetRAFTKineticFitting():
         return loss
     
     def display_overlay(self, new_k):
-        k_AB, k_BA = new_k
-        k_AA = 1
-        k_BB = 1
+        k_AA, k_BB = new_k
+        k_AB = 1.
+        k_BA = 1.
 
         sol = self._integrate_ODE(k_AA, k_AB, k_BA, k_BB)
         pred_F, pred_X = self._convert_XF(sol)
@@ -116,8 +116,8 @@ class PetRAFTKineticFitting():
         plt.show()
     
     def predict_conversion(self, r_A, r_B):
-        k_AB = 1
-        k_BA = 1
+        k_AB = 1.
+        k_BA = 1.
         k_AA = r_A
         k_BB = r_B
 
@@ -139,10 +139,10 @@ class PetRAFTKineticFitting():
         return new_k.x
     
     def test_values(self, r_1, r_2):
-        k_AB = 1/r_1
-        k_BA = 1/r_2
-        k_AA = 1.
-        k_BB = 1.
+        k_AB = 1.
+        k_BA = 1.
+        k_AA = r_1
+        k_BB = r_2
 
         sol = self._integrate_ODE(k_AA, k_AB, k_BA, k_BB)
         pred_F, pred_X = self._convert_XF(sol)
